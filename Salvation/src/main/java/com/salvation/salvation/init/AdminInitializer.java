494baa1e503b36f13aea29c58c9ece232ff1f4a8
Salvador <abdellahelghoulamdev@gmail.com>
2025-05-05 00:05:24 +0100

    SCRUM-17 : Add role-based access for /admin/** routes

--- Salvation/src/main/java/com/salvation/salvation/init/AdminInitializer.java
+++ Salvation/src/main/java/com/salvation/salvation/init/AdminInitializer.java
@@ -4,6 +4,9 @@ import com.salvation.salvation.model.Role;
 import com.salvation.salvation.model.User;
 import com.salvation.salvation.repository.RoleRepository;
 import com.salvation.salvation.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,13 @@ import java.util.Collections;
 
 @Component
 public class AdminInitializer implements CommandLineRunner {
+    @Value("${admin.username}")
+    private String userName;
+    @Value("${admin.password}")
+    private String password;
+
+    private static final Logger logger = LoggerFactory.getLogger(AdminInitializer.class);
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
@@ -24,12 +34,12 @@ public class AdminInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        if (userRepository.findByUsername("admin").isEmpty()) {
+        if (userRepository.findByUsername(userName).isEmpty()) {
             Role adminRole = roleRepository.findByName("ROLE_ADMIN").orElse(roleRepository.save(Role.builder().name("ROLE_ADMIN").build()));
 
             User admin = User.builder()
-                    .username("admin")
-                    .password(passwordEncoder.encode("admin123"))
+                    .username(userName)
+                    .password(passwordEncoder.encode(password))
                     .roles(Collections.singleton(adminRole))
                     .enabled(true)
                     .accountNonExpired(true)
@@ -38,6 +48,7 @@ public class AdminInitializer implements CommandLineRunner {
                     .build();
 
             userRepository.save(admin);
+            logger.info("Admin user created with username: admin");
         }
     }
 
